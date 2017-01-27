9192efa88b5bac8a2f569ff3e12257e2085542a5
Sean Walsh <sdwalsh@mirango.io>
2017-01-27 01:40:13 -0500

    create graph type and addVertex and getVertexList methods

--- src/io/mirango/Graph.java
+++ src/io/mirango/Graph.java
@@ -1,7 +1,22 @@
 package io.mirango;
 
-public class Graph<T> {
-    private Graph() {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Graph<T> {
+    private List<Vertex<T>> vertexList;
+
+    public Graph(T v) {
+        this.vertexList = new ArrayList<>();
+    }
+
+    public boolean addVertex(T type) {
+        Vertex<T> v = new Vertex<>(type);
+        vertexList.add(v);
+        return true;
+    }
+
+    public List<Vertex<T>> getVertexList() {
+        return vertexList;
     }
 }
