78359a426a1e9d1a18ab3eea18a8a6bb39b0d86e
Sean Walsh <sdwalsh@mirango.io>
2017-02-05 22:14:48 -0500

    Add to readme

--- plot.r
+++ plot.r
@@ -8,9 +8,15 @@ m <-lm(y ~ x, data=d)
 abline(coef(m), col="blue")
 pr.abline <- predict(m)
 
+fit1 <- lm(y~poly(x,5,raw=TRUE), data=d)
+
 plot(y ~ x, data=d, xlab="vertices", ylab="largest edge in mst", main="Largest Edge by Number of Vertices")
 lines(pr.loess~x, data=d, col="red", lwd=2)
 lines(pr.abline~x, data=d, col="blue", lwd=2)
 
+xx <- seq(0,3000, length=100)
+lines(xx, predict(fit1, data.frame(x=xx)), col="green", lwd=2)
+
 summary(pr.abline)
 summary(pr.loess)
+summary(fit1)
